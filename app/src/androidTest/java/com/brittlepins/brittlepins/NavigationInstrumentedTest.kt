72b145863407d642d2ee3f8f0f56ff239b2030d3
EN <ekaterina.v.nikonova@gmail.com>
2020-07-14 16:47:34 +0200

    Back press in Projects sends to background (cannot test)

--- app/src/androidTest/java/com/brittlepins/brittlepins/NavigationInstrumentedTest.kt
+++ app/src/androidTest/java/com/brittlepins/brittlepins/NavigationInstrumentedTest.kt
@@ -8,7 +8,7 @@ import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
--- app/src/main/java/com/brittlepins/brittlepins/main/ui/projects/ProjectsFragment.kt
+++ app/src/main/java/com/brittlepins/brittlepins/main/ui/projects/ProjectsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -27,5 +28,12 @@ class ProjectsFragment : Fragment() {
             textView.text = it
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback {
+            requireActivity().moveTaskToBack(true)
+        }
     }
 }
