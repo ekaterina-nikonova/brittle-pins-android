d00d05895d039251081e5604bd3fabde3c3feb94
EN <ekaterina.v.nikonova@gmail.com>
2020-07-15 07:36:34 +0200

    Create log in form with username edit text; write test

---
+++ app/src/androidTest/java/com/brittlepins/brittlepins/LogInInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.brittlepins.brittlepins
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.brittlepins.brittlepins.authentication.login.LogInFragment
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LogInInstrumentedTest {
+    @Test
+    fun logInScreenHasUsernameOrEmailEditText() {
+        launchFragmentInContainer<LogInFragment>()
+        onView(withId(R.id.log_in_form))
+            .check(matches(withChild(withHint("Username or email"))))
+    }
+}
