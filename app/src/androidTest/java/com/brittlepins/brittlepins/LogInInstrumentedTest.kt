2c865db0a56cd872aa46de39df5a4d19c993ed04
EN <ekaterina.v.nikonova@gmail.com>
2020-07-15 07:46:59 +0200

    Replace hard-coded string in test with reference to resources

--- app/src/androidTest/java/com/brittlepins/brittlepins/LogInInstrumentedTest.kt
+++ app/src/androidTest/java/com/brittlepins/brittlepins/LogInInstrumentedTest.kt
@@ -1,20 +1,25 @@
 package com.brittlepins.brittlepins
 
+import android.content.res.Resources
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.brittlepins.brittlepins.authentication.login.LogInFragment
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LogInInstrumentedTest {
+    private val resources: Resources =
+        InstrumentationRegistry.getInstrumentation().targetContext.resources
+
     @Test
     fun logInScreenHasUsernameOrEmailEditText() {
         launchFragmentInContainer<LogInFragment>()
         onView(withId(R.id.log_in_form))
-            .check(matches(withChild(withHint("Username or email"))))
+            .check(matches(withChild(withHint(resources.getString(R.string.log_in_username_hint)))))
     }
 }
